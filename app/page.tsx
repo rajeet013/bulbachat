d08bd671900bce1f7cd4dadae4ab3bb5c87326a2
Rajeet013 <rajeet013@gmail.com>
2026-05-26 02:34:32 +0600

    Authenticated

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { currentUser } from "@/modules/authentication/actions";
+import UserButton from "@/modules/authentication/components/user-button";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+
+  const user = await currentUser();
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <Button>Hello World</Button>      
+      <UserButton user={user} />      
     </div>
   );
 }
