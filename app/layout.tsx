c9675dafd0c8cdc17c62141f5365ba299a4196fa
Rajeet013 <rajeet013@gmail.com>
2026-05-27 08:05:47 +0600

    Done

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
@@ -31,6 +32,7 @@ export default function RootLayout({
       lang="en"
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
     suppressHydrationWarning>
+      <QueryProvider>
        <ThemeProvider
           attribute="class"
           defaultTheme="system"
@@ -39,6 +41,7 @@ export default function RootLayout({
         >
       <body className="min-h-full flex flex-col">{children}</body>
       </ThemeProvider>
+      </QueryProvider>
     </html>
   );
 }
